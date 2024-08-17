0d4a3aca6983c104165084ebb2d5da63e5499e79
Dylan Provoost <dylanprovoost@hotmail.com>
2024-08-17 16:05:42 +0200

    Setting up formdata retrieval

--- src/Core/Request.php
+++ src/Core/Request.php
@@ -5,6 +5,7 @@ namespace Phabrique\Core;
 interface Request
 {
     public function getQueryParameters(): array;
+    public function getFormData(): array;
     public function getPath(): string;
     public function getMethod(): string;
 }
--- tests/Core/ServerRequestTest.php
+++ tests/Core/ServerRequestTest.php
@@ -3,10 +3,19 @@
 declare(strict_types=1);
 
 use Phabrique\Core\ServerRequest;
+use PHPUnit\Framework\Attributes\After;
 use PHPUnit\Framework\TestCase;
 
 final class ServerRequestTest extends TestCase
 {
+    #[After]
+    public function resetGlobals()
+    {
+        $_GET = [];
+        $_SERVER = [];
+        $_POST = [];
+    }
+
     public function testRequestObjectContainsProperUriFromHttpRequest(): void
     {
         $_SERVER["REQUEST_URI"] = "/endpoint.php";
@@ -16,7 +25,8 @@ final class ServerRequestTest extends TestCase
         $this->assertEquals("/endpoint.php", $request->getPath());
     }
 
-    public function testRequestContainsProperLowercaseHttpMethod() {
+    public function testRequestContainsProperLowercaseHttpMethod()
+    {
         $_SERVER["REQUEST_URI"] = "/endpoint.php";
         $_SERVER["REQUEST_METHOD"] = "GET";
 
@@ -30,21 +40,23 @@ final class ServerRequestTest extends TestCase
         $_SERVER["REQUEST_URI"] = "/endpoint.php";
         $_SERVER["REQUEST_METHOD"] = "GET";
 
-        $_GET = ["param1" => "value1", "param2" => "value2"];
+        $_GET["param1"] = "value1";
+        $_GET["param2"] = "value2";
         $request = ServerRequest::parse();
 
         $this->assertTrue(array_key_exists("param1", $request->getQueryParameters()));
         $this->assertEquals("value1", $request->getQueryParameters()["param1"]);
     }
 
-    public function testRequestDoesntContainUnknownQueryParam() {
+    public function testRequestContainsFormParam()
+    {
         $_SERVER["REQUEST_URI"] = "/endpoint.php";
         $_SERVER["REQUEST_METHOD"] = "GET";
 
-        $_GET = ["param1" => "value1"];
+        $_POST["param1"] = "value1";
         $request = ServerRequest::parse();
 
-        $this->assertFalse(array_key_exists("param2", $request->getQueryParameters()));
-
+        $this->assertTrue(array_key_exists("param1", $request->getFormData()));
+        $this->assertEquals("value1", $request->getFormData()["param1"]);
     }
 }
