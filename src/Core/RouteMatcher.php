39fb172eb850c58cf4dbb2410237e085259f0bed
Pol Lefèvre <pollefev@gmail.com>
2024-08-18 13:47:52 +0200

    Add implementation for a RouteMatcher

---
+++ src/Core/RouteMatcher.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phabrique\Core;
+
+use Exception;
+
+
+const TEMPLATE_PATTERN = "/^(\/:?[a-zA-Z_-][0-9a-zA-Z_-]*)+$/";
+const PATH_PATTERN = "/^(\/[0-9a-zA-Z_-]+)+$/"; // Same but without ':'
+
+class RouteMatcher
+{
+    private ?array $lastMatch = null;
+
+    public static function compile(string $template): RouteMatcher
+    {
+        if (!preg_match(TEMPLATE_PATTERN, $template)) {
+            throw new Exception("Invalid template '$template'");
+        }
+        $parts = array_slice(explode("/", $template), 1);
+
+        // Assert no multiple keys
+        foreach ($parts as $part) {
+            if ($part[0] == ':') {
+                if (array_count_values($parts)[$part] > 1) {
+                    $key = substr($part, 1);
+                    throw new Exception("Duplicate key '$key'");
+                }
+            }
+        }
+
+        return new RouteMatcher($parts);
+    }
+
+    private function __construct(private array $parts) {}
+
+    public function matches(string $path): bool
+    {
+        $this->lastMatch == null;
+
+        if (!preg_match(PATH_PATTERN, $path)) {
+            $this->lastMatch = null;
+            throw new Exception("Invalid path '$path'");
+        }
+
+        $pathParts = array_slice(explode("/", $path), 1);
+        if (count($pathParts) != count($this->parts)) {
+            return false;
+        }
+
+        $lastMatch = [];
+        for ($i = 0; $i < count($this->parts); $i++) {
+            if ($this->parts[$i][0] == ':') {
+                $key = substr($this->parts[$i], 1);
+                $lastMatch[$key] = $pathParts[$i];
+            } else {
+                if ($pathParts[$i] != $this->parts[$i]) {
+                    return false;
+                }
+            }
+        }
+        $this->lastMatch = $lastMatch;
+        return true;
+    }
+
+    public function extract(): array
+    {
+        if (is_null($this->lastMatch)) {
+            throw new Exception("Cannot extract path params from unmatched path");
+        }
+        return $this->lastMatch;
+    }
+}
---
+++ tests/Core/RouteMatcherTest.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+use Phabrique\Core\RouteMatcher;
+use PHPUnit\Framework\TestCase;
+
+class RouteMatcherTest extends TestCase
+{
+
+    function setUp(): void
+    {
+        # Turn on error reporting
+        error_reporting(E_ALL);
+    }
+
+    function testRouteMatcherWithoutParamsMatchesCorrectPath()
+    {
+        $m = RouteMatcher::compile("/my/path");
+        $this->assertTrue($m->matches("/my/path"));
+    }
+
+    function testRouteMatcherWithoutParamsDoesntMatchIncorrectPath()
+    {
+        $m = RouteMatcher::compile("/my/path");
+        $this->assertFalse($m->matches("/not/my/path"));
+    }
+
+    function testSimpleRouteMatcherReturnsEmptyPathParams()
+    {
+        $m = RouteMatcher::compile("/my/path");
+        $this->assertTrue($m->matches("/my/path"));
+        $this->assertEmpty($m->extract());
+    }
+
+    function testRouteMatcherParsesPathParams()
+    {
+        $m = RouteMatcher::compile("/items/:id");
+        $m->matches("/items/69");
+        $this->assertEquals(["id" => "69"], $m->extract());
+    }
+
+    function testCannotExtractIfDidntMatch()
+    {
+        $m = RouteMatcher::compile("/items/:id");
+        $m->matches("/other-items/69");
+
+        try {
+            $m->extract();
+            $this->fail('$m->extract() was supposed to throw');
+        } catch (Exception $e) {
+            $this->assertEquals("Cannot extract path params from unmatched path", $e->getMessage());
+        }
+    }
+
+    function testInvalidTemplateThrows()
+    {
+        try {
+            RouteMatcher::compile("invalid");
+            $this->fail("matcher compilation was supposed to throw");
+        } catch (Exception $e) {
+            $this->assertEquals("Invalid template 'invalid'", $e->getMessage());
+        }
+    }
+
+    function testTemplateWithMultipleSameKeyThrows()
+    {
+        try {
+            RouteMatcher::compile("/users/:id/friends/:id");
+            $this->fail("matcher compilation was supposed to throw");
+        } catch (Exception $e) {
+            $this->assertEquals("Duplicate key 'id'", $e->getMessage());
+        }
+    }
+
+    function testInvalidPathThrows()
+    {
+        try {
+            $m = RouteMatcher::compile("/items/:id/price");
+            $m->matches("items 52");
+            $this->fail('$m->matches() was supposed to throw');
+        } catch (Exception $e) {
+            $this->assertEquals("Invalid path 'items 52'", $e->getMessage());
+        }
+    }
+}
